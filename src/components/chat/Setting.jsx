b1bf8919ca1a049fbe44573c91f820f58407b7d9
etabebe1 <etabebe1@gmail.com>
2024-03-19 08:54:53 -0400

    third form completed

--- src/components/chat/Setting.jsx
+++ src/components/chat/Setting.jsx
@@ -32,7 +32,7 @@ function Setting() {
       <div
         aria-label="Setting Container"
         role="complementary"
-        className="setting-container space-y-5 overflow-y-auto"
+        className="setting-container space-y-5 overflow-y-auto mb-20 pb-[3rem]"
         style={{ height: "100%" }}
       >
         {/* initial  form container */}
@@ -233,6 +233,95 @@ function Setting() {
                       id="address"
                       name="address"
                       value={formData.email}
+                      onChange={handleChange}
+                      className="mt-1 block w-[100%] px-3 py-2 bg-colors-quantum-silver/60 text-white/85 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              {/* button container */}
+              <div className="flex gap-4 justify-between sm:justify-start px-4">
+                <button
+                  type="submit"
+                  className="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 cursor-pointer"
+                >
+                  Update
+                </button>
+                <button
+                  type="button"
+                  onClick={handleCancel}
+                  className="px-4 py-2 bg-gray-600 text-white rounded-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 cursor-pointer"
+                >
+                  Cancel
+                </button>
+              </div>
+            </form>
+          </section>
+        </div>
+
+        {/* third form container */}
+        <div className="w-[90%] sm:w-full h-[30rem] sm:h-[63vh] bg-colors-digital-gray/50 px-5 rounded-lg mx-auto my-5 sm:mt-0">
+          <header className="top-element text-white text-sm sm:text-base">
+            <div className="flex flex-row items-center justify-between py-5">
+              <h1 className="font-bold text-sm sm:text-lg">Change Password</h1>
+            </div>
+            <hr className="border-1 border-colors-quantum-silver w-full" />
+          </header>
+
+          <section
+            aria-label=""
+            className="body-element sm:px-5"
+            style={{ flex: 1, height: "0px" }}
+          >
+            <form onSubmit={handleSubmit} className="account-update-form ">
+              {/* input container */}
+              <div className="form-inputs flex border-b-1 border-colors-quantum-silver py-5 my-5">
+                <div className="w-full flex flex-wrap gap-4 justify-center sm:justify-start">
+                  <div className="min-w-[15rem] sm:w-[48%]">
+                    <label
+                      htmlFor="oldPassword"
+                      className="block text-sm font-medium text-white"
+                    >
+                      Old Password
+                    </label>
+                    <input
+                      type="password"
+                      id="oldPassword"
+                      name="oldPassword"
+                      value={formData.firstName}
+                      onChange={handleChange}
+                      className="mt-1 block w-[100%] px-3 py-2 bg-colors-quantum-silver/60 text-white/85 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                    />
+                  </div>
+                  <div className="min-w-[15rem] sm:w-[48%]">
+                    <label
+                      htmlFor="newPassword"
+                      className="block text-sm font-medium text-white"
+                    >
+                      New Password
+                    </label>
+                    <input
+                      type="password"
+                      id="newPassword"
+                      name="newPassword"
+                      value={formData.firstName}
+                      onChange={handleChange}
+                      className="mt-1 block w-[100%] px-3 py-2 bg-colors-quantum-silver/60 text-white/85 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                    />
+                  </div>
+                  <div className="min-w-[15rem] sm:w-[48%]">
+                    <label
+                      htmlFor="confirmPassword"
+                      className="block text-sm font-medium text-white"
+                    >
+                      Confirm Password
+                    </label>
+                    <input
+                      type="password"
+                      id="confirmPassword"
+                      name="confirmPassword"
+                      value={formData.firstName}
                       onChange={handleChange}
                       className="mt-1 block w-[100%] px-3 py-2 bg-colors-quantum-silver/60 text-white/85 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
                     />
