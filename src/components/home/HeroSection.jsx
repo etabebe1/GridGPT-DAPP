6546fc191fb65d3180caad8ede87515879d1fb9c
etabebe1 <etabebe1@gmail.com>
2024-03-28 01:49:06 -0400

    hand and laptop style completed

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -11,7 +11,7 @@ function HeroSection() {
         height: "calc(200vh - 64px)",
       }}
     >
-      <section className="hero-container max-w-4xl w-full h-full flex justify-center">
+      <section className="hero-container max-w-4xl w-[80vw] h-full flex justify-center">
         <div className="home-content w-full h-full relative">
           <div className="bg-effect z-0">
             <img
@@ -20,8 +20,9 @@ function HeroSection() {
               alt=""
             />
           </div>
-          <div className="top-layer absolute top-10 z-20 flex flex-col justify-center w-full px-2">
-            <div className="flex flex-col justify-center items-center text-center">
+          <div className="content-layer absolute top-10 z-20 flex flex-col justify-center w-full px-2 gap-10 ">
+            {/* text */}
+            <div className="text-layer flex flex-col justify-center items-center text-center">
               <h1 className="flex flex-wrap justify-center items-center text-4xl font-semibold mb-10">
                 <span>USE MEGABOT TO</span>
                 <div className="relative">
@@ -30,7 +31,7 @@ function HeroSection() {
                     className="h-[80px] w-[256px]"
                     alt=""
                   />
-                  <span className="absolute top-[25%] right-[6.5%] bg-gray-600/50 backdrop-blur-2xl  shadow-lg px-4 text-colors-black-hole text-transparent">
+                  <span className="absolute top-[25%] right-[6.5%] bg-gray-600/50 backdrop-blur-2xl  shadow-lg px-4 text-colors-black-hole">
                     SIMPLIFY
                   </span>
                 </div>
@@ -51,9 +52,43 @@ function HeroSection() {
                 </div>
               </div>
             </div>
+
+            {/* image */}
+            <div className="laptop-layer">
+              <div className="hand-container relative">
+                <img
+                  src="/assets/images/home/laptop1.png"
+                  className="laptop w-full border-8 border-gray-600/70 shadow-md shadow-gray-600/70 rounded-2xl z-40"
+                  alt=""
+                />
+
+                <img
+                  src="/assets/images/home/hand.png"
+                  className="left-hand absolute -bottom-[25rem] -left-36 -z-10 hidden sm:block"
+                  alt=""
+                />
+
+                <img
+                  src="/assets/images/home/hand.png"
+                  className="right-hand absolute -bottom-[25rem] -right-36 -z-10 transform -scale-x-100 hidden sm:block"
+                  alt=""
+                />
+
+                <img
+                  src="/assets/images/home/finger.png"
+                  className="absolute -bottom-10 -left-10 hidden sm:block"
+                  alt=""
+                />
+
+                <img
+                  src="/assets/images/home/finger.png"
+                  className="absolute -bottom-10 -right-10 -scale-x-100 hidden sm:block"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="bottom"></div>
           </div>
-          <div className="middle">dhfkjhadsj</div>
-          <div className="bottom"></div>
         </div>
       </section>
     </div>
